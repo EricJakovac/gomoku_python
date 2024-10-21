2357aa77f7e8c4bf3a816474ee3eeaa580ac1446
EricJakovac <jakovaceric@gmail.com>
2024-10-21 23:40:45 +0200

    Helper funkcije gotove

--- agent_script.py
+++ agent_script.py
@@ -1,6 +1,6 @@
-import config
 import random
 
+import config
 
 DEPTH = config.DEPTH
 
@@ -15,31 +15,54 @@ elif config.BOARD_SIZE == "tiny":
 
 # Evaluates the current state of the game board and returns a score that represents the desirability of the state for the maximizing player
 def evaluate_state(board_matrix, player, is_max_player):
-    # (an example of how to implement this function)
-    # similar to the function `is_terminal_state`,
-    #     but instead of just checking if there is a line with 5 `player` stones,
-    #     it has to count the number of `player` stones in each straight line with length 5
-    #     Ignore lines for which it is impossible to have 5 `player` stones in the future
-    #         (e.g., because there is an enemy stone in that line)
-    # the stone count is the score
-    #     but if the current player is the maximizing player the score is positive
-    #     and if the current player is the minimizing player the score is negative
-    # return score
-    pass
+    score = 0
+
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            if board_matrix[i][j] == player:
+                for dx, dy in [(1, 0), (0, 1), (1, 1), (1, -1)]:
+                    count = 0
+                    for k in range(5):
+                        x, y = i + dx * k, j + dy * k
+                        if 0 <= x < BOARD_SIZE and 0 <= y < BOARD_SIZE:
+                            if board_matrix[x][y] == player:
+                                count += 1
+                            elif board_matrix[x][y] != 0:
+                                break
+                        else:
+                            break
+                    if count > 0:
+                        score += count if is_max_player else -count
+    return score
 
 
 # Checks if a given state is a terminal state (i.e., if the game is over: somebody won or the board is full and nobody won)
-def is_terminal_state(board_matrix, player, is_max_player):
-    # check if there is at least one line with 5 `player` stones
-    #     (it is enough to check only the stones of the player who played last: `player`)
-    #     lines can be: horizontal, vertical, or diagonal (two diagonals: / and \)
-    # terminal_state is true or false (true if there is at least one line with 5 `player` stones) and false otherwise
-    # score is 0, float("inf"), or float("-inf")
-    #       0 if nobody won (the board is full)
-    #    +inf if the maximizing player won
-    #    -inf if the minimizing player won
-    # return terminal_state, score
-    pass
+def is_terminal_state(board_matrix, player):
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            if board_matrix[i][j] == player:
+                # Horizontalno
+                if j + 4 < BOARD_SIZE and all(board_matrix[i][j + k] == player for k in range(5)):
+                    return True, float("inf")
+                # Vertikalno
+                if i + 4 < BOARD_SIZE and all(board_matrix[i + k][j] == player for k in range(5)):
+                    return True, float("inf")
+                # Dijagonala /
+                if i - 4 >= 0 and j + 4 < BOARD_SIZE and all(board_matrix[i - k][j + k] == player for k in range(5)):
+                    return True, float("inf")
+                # Dijagonala \
+                if (
+                    i + 4 < BOARD_SIZE
+                    and j + 4 < BOARD_SIZE
+                    and all(board_matrix[i + k][j + k] == player for k in range(5))
+                ):
+                    return True, float("inf")
+
+    # Provjera je li ploča puna
+    if all(board_matrix[i][j] != 0 for i in range(BOARD_SIZE) for j in range(BOARD_SIZE)):
+        return True, 0  # Izjednačeno
+
+    return False, None
 
 
 # The minimax recursive function, returns the state's score
@@ -65,24 +88,31 @@ def minimax(board_matrix, depth, is_max_player, player):
 
 # Modifies the given state (board_matrix) to make the specified move for the current player
 def make_move(board_matrix, player, move):
-    # set the `move` coordinate in the board_matrix to -1 or 1 (player)
-    pass
+    x, y = move
+    board_matrix[x][y] = player
+
 
 # Removes the last move
-def remove_move(board_matrix, player, move):
-    # set the `move` coordante in the board_matrix to 0
-    pass
+def remove_move(board_matrix, move):
+    x, y = move
+    board_matrix[x][y] = 0
 
 
 # Returns a list of valid moves (represented as tuples of the x and y coordinates) for the current player in the given state
 #   All possible moves (empty positions)
 def get_valid_moves(board_matrix):
-    pass
+    valid_moves = []
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            if board_matrix[i][j] == 0:
+                valid_moves.append((i, j))
+    return valid_moves
 
 
 def main(board_matrix, player):
-    y = round(random.random() * (BOARD_SIZE-1))
-    x = round(random.random() * (BOARD_SIZE-1))
+    y = round(random.random() * (BOARD_SIZE - 1))
+    x = round(random.random() * (BOARD_SIZE - 1))
+    print(get_valid_moves(board_matrix))
     """
     ### search for the best move with minimax algorithm
     best_score = float("-inf")
